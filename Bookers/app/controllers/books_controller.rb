2ff294ff9656e3b284ea10c09d96967ce9e16f68
vagrant <vagrant@localhost.localdomain>
2020-08-07 05:18:09 +0000

    [Finish] 4-8

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -2,7 +2,11 @@ class BooksController < ApplicationController
 	def top
 	end
 
-	
+	def index
+	end
+
+	def edit
+	end
 
 
 	
--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'newbook/new'
-  get '/index' => 'posts#index'
+  get '/index' => 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'books#top'
-  
+  post '/edit' => 'books#edit'
 end
